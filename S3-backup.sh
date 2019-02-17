4150145b59a03304f7b958266b654cb0941ad884
aorin <aorin@users.noreply.github.com>
2019-02-17 14:01:04 +0200

    Update S3-backup.sh

--- S3-backup.sh
+++ S3-backup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 project_path=$(dirname "$0")
 source $project_path/config.sh
-$project_path/env/bin/aws s3 sync $directory s3://$bucket_name
+$project_path/env/bin/aws s3 sync --delete $directory s3://$bucket_name
